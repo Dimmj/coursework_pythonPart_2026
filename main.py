d31ccfaca454fc61dbcea23317f5204063bc19fe
Dimmj <118057923+Dimmj@users.noreply.github.com>
2026-05-18 12:51:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from handler_json import Handler
+from handler_rabbit import RabbitMQHandler
+
+
+if __name__ == "__main__":
+    # Путь к модели
+    MODEL_PATH = "model/sales_model_pickle.pkl"
+
+    handler = Handler(model_path=MODEL_PATH)
+
+    # Создаем RabbitMQ обработчик
+    rabbit_handler = RabbitMQHandler(handler)
+
+    try:
+        # Подключаемся
+        rabbit_handler.connect()
+
+        # Запускаем прослушивание
+        rabbit_handler.start_consuming()
+
+    except Exception as e:
+        print(f"ошибка: {e}")
+        rabbit_handler.close()
